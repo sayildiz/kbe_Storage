29fd8c67f81ee4fc3171aef549af05d0cc4d32bb
sayildiz <y.samet97@gmail.com>
2021-12-10 16:51:22 +0100

    project init

---
+++ src/main/java/com/sayildiz/kbe_storage/KbeStorageApplication.java
@@ -0,0 +1,13 @@
+package com.sayildiz.kbe_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbeStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sayildiz/kbe_storage/KbeStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayildiz.kbe_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
