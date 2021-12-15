45e8e34b05c50e43ad7afbbed076e74e01306ccb
sayildiz <y.samet97@gmail.com>
2021-12-15 18:42:33 +0100

    Error Message on invalid UUID input on GET /productinfo/{uuid}

--- src/main/java/com/sayildiz/kbe_storage/product_api/controller/ProductInfoController.java
+++ src/main/java/com/sayildiz/kbe_storage/product_api/controller/ProductInfoController.java
@@ -2,9 +2,9 @@ package com.sayildiz.kbe_storage.product_api.controller;
 
 import com.sayildiz.kbe_storage.product_api.model.ProductInfo;
 import com.sayildiz.kbe_storage.product_api.service.ProductInfoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.UUID;
@@ -27,5 +27,12 @@ public class ProductInfoController {
     @GetMapping("/productinfo/{uuid}")
     public ProductInfo getProductInfoById(@PathVariable UUID uuid){
         return productInfoService.getByUuid(uuid);
+    }
+
+   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentTypeMismatchException() {
+        return "Bad Request: expects valid UUID like 9ff7de0c-acc9-4cee-a99c-c6fbafa237c9";
     }
 }
